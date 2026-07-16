e505010e4d24a1114dbd664f2ea1598f3cdbfe6a
SiebeGeurts <siebe.geurts@epfl.ch>
2026-07-16 10:39:48 +0200

    unimportant files for transfering to new machine

--- directory_paths.py
+++ directory_paths.py
@@ -1,7 +1,7 @@
 # path where the data is stored
-PARENT_DIR = "/media/data16/adriana/ProprioPerception/0_Zenodo"
+PARENT_DIR = "/media1/siebe/datasets/"
 
 # path where the data is stored to save the results if different
 SAVE_DIR = PARENT_DIR
 
-MODELS_DIR = "trained_models"
+MODELS_DIR = "trained_models_myo"
